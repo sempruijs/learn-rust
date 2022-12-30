67b6783734239a0f3047d888e58e7b29e9db8c0c
Sem Pruijs <sem@pruijs.net>
2022-12-30 12:56:39 +0100

    make get_fibonacci

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     let temperture = fahrenheit_to_celcius(-40.0);
-    print!("{}", temperture);
+    let number = get_fibonacci(4);
+    print!("{temperture}\n");
+
+    print!("{number}\n");
 }
 
 fn fahrenheit_to_celcius(t: f64) -> f64 {
     ((t - 32.0) * 5.0) / 9.0
+}
+
+fn get_fibonacci(n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        1
+    } else {
+        get_fibonacci(n - 2) + get_fibonacci(n - 1)
+    }
 }
