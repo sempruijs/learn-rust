dddbcf4adfde17436cdd91c2ef21d311320c8123
Sem Pruijs <sem@pruijs.net>
2022-12-30 12:23:03 +0100

    Create fahrnheit function

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let temperture = fahrenheit_to_celcius(-40.0);
+    print!("{}", temperture);
+}
+
+fn fahrenheit_to_celcius(t: f64) -> f64 {
+    ((t - 32.0) * 5.0) / 9.0
 }
