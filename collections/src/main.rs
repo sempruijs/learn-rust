f9b84af3c2dd5e2cd9f9e70b031fcf882e7a1749
sempruijs <sem@pruijs.net>
2023-01-06 21:49:54 +0100

    Add rust book as markdown

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,23 +1,20 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 100];
+    let name = "sem";
+    let c = name.chars().nth(1).unwrap();
 
-    vector::show_vector(&v);
-
-    vector::add_fifty(&mut v);
-
-    vector::show_vector(&v);
+    println!("{}", c)
 }
 
-mod vector {
-    pub fn show_vector(v: &Vec<i32>) {
-        for i in v {
-            print!("{}\n", i);
-        }
-    }
+// mod vector {
+//     pub fn show_vector(v: &Vec<i32>) {
+//         for i in v {
+//             print!("{}\n", i);
+//         }
+//     }
 
-    pub fn add_fifty(v: &mut Vec<i32>) {
-        for i in v {
-            *i += 50;
-        }
-    }
-}
+//     pub fn add_fifty(v: &mut Vec<i32>) {
+//         for i in v {
+//             *i += 50;
+//         }
+//     }
+// }
