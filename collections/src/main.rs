5730da60871dd4d5c715b82b361c4673ab1d1fba
sempruijs <sem@pruijs.net>
2023-01-07 17:22:05 +0100

    Make median function

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,8 +1,16 @@
-fn main() {
-    let name = "sem";
-    let c = name.chars().nth(1).unwrap();
+// use std::collections::HashMap;
+// use std::collections;
 
-    println!("{}", c)
+fn main() {
+    let x = median(vec![1, 2, -3, 4, 5]);
+    println!("{}", x);
+}
+
+// median is the middle number in a list when sorted.
+fn median(v: Vec<i32>) -> i32 {
+    // if len is even, it will ceil it.
+    let middel_index = v.len() / 2;
+    v[middel_index]
 }
 
 // mod vector {
