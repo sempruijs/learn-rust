3edf8e9eec1f42ff354e1cfa4b9ecc11b2c314da
sempruijs <sem@pruijs.net>
2023-01-07 22:56:28 +0100

    make modus function

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -2,15 +2,26 @@
 // use std::collections;
 
 fn main() {
-    let x = median(vec![1, 2, -3, 4, 5]);
-    println!("{}", x);
+    let numbers = [1, 2, 3, 9];
+    let modes_of_numbers = modus(&numbers);
+    println!("{}", modes_of_numbers);
 }
 
 // median is the middle number in a list when sorted.
-fn median(v: Vec<i32>) -> i32 {
+fn median(v: &[i32]) -> i32 {
     // if len is even, it will ceil it.
     let middel_index = v.len() / 2;
     v[middel_index]
+}
+
+fn modus(v: &[i32]) -> f32 {
+    let mut total = 0;
+
+    for i in v {
+        total += i;
+    }
+
+    total as f32 / v.len() as f32
 }
 
 // mod vector {
