65ec8a4c0a385757885125fd3d77b211938f0515
sempruijs <sem@pruijs.net>
2023-01-19 18:30:59 +0100

    finish chapter 9

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     let numbers = [1, 3, 4, 100, 3];
     let word = vec!['a', 'b', 'z', 'd'];
@@ -7,6 +9,15 @@ fn main() {
 
     println!("{}", largest_from_numbers);
     println!("{}", largest_from_word);
+
+    let point = Point::new(5, 6);
+    println!("{:?}", point);
+
+    let s1 = String::from("short");
+    let s2 = String::from("very long string. long.");
+
+    let result = longest_with_announcement(&s1, &s2, "here is the longest string!!!!");
+    println!("{}", result);
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
@@ -19,4 +30,25 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     }
 
     largest
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+fn longest_with_announcement<'a, T: Display>(s1: &'a str, s2: &'a str, announcement: T) -> &'a str {
+    println!("announcement: {}", announcement);
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
 }
