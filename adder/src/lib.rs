2fefa615af2ec8c67b38c01520bdae69a7236706
sempruijs <sem@pruijs.net>
2023-01-25 16:09:51 +0100

    finish 11.1

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,3 +1,27 @@
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    fn new(x: i32) -> Guess {
+        if x < 0 {
+            panic!("guess should be equal or above 0. got {}", x);
+        } else if x > 100 {
+            panic!("guess should be equal or below 100. Got {}", x);
+        }
+
+        Guess { value: x as u32 }
+    }
+}
+
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+fn salut_name(n: &str) -> String {
+    format!("salut {}", n)
+}
+
 #[derive(Debug)]
 struct Rectangle {
     x: u32,
@@ -20,5 +44,31 @@ mod tests {
         let smaller = Rectangle { x: 3, y: 3 };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn it_add_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn salut_coreect() {
+        let result = salut_name("harko");
+        assert!(result.contains("salut harko"), "result was {}", result);
+    }
+
+    #[test]
+    #[should_panic(expected = "equal or below 100")]
+    fn greater_than_100() {
+        Guess::new(101);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 2 doesn't match 4"))
+        }
     }
 }
