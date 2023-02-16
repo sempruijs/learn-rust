8b173076161c9a648b8bf71aaca3a3677d774762
sempruijs <sem@pruijs.net>
2023-02-16 23:59:02 +0100

    finish chapter 15

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -1,4 +1,6 @@
+use crate::List::{Cons, Nil};
 use std::ops::Deref;
+use std::rc::Rc;
 
 struct MyBox<T>(T);
 
@@ -6,6 +8,21 @@ impl<T> MyBox<T> {
     pub fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("out of scope. Had data: {}", self.data);
+    }
+}
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
 impl<T> Deref for MyBox<T> {
@@ -29,4 +46,36 @@ fn main() {
 
     let n = MyBox(String::from("Harko"));
     print_name(&n);
+
+    let a = CustomSmartPointer {
+        data: String::from("1"),
+    };
+
+    let b = CustomSmartPointer {
+        data: String::from("2"),
+    };
+
+    let c = CustomSmartPointer {
+        data: String::from("3"),
+    };
+
+    println!("created Custom Smart pointers");
+
+    drop(b);
+
+    let m = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("a has {} references", Rc::strong_count(&m));
+
+    let n = Cons(2, Rc::clone(&m));
+
+    println!("a has {} references", Rc::strong_count(&m));
+    {
+        let o = Cons(4, Rc::clone(&m));
+
+        println!("a has {} references", Rc::strong_count(&m));
+    }
+
+    println!("a has {} references", Rc::strong_count(&m));
+
+    println!("a has {} references", Rc::strong_count(&m));
 }
